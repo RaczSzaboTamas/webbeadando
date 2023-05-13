e9a6a90aa228fde1d3d493b39605f0ed902aec6c
Fozo Attila <attilafozo@hotmail.com>
2023-05-13 23:18:58 +0200

    Adding user login system commit 3

---
+++ user_login/config.php
@@ -0,0 +1,8 @@
+<?php
+
+$db_user = "lihanaur";
+$db_pass = "123456";
+$db_name = "lihanaur";
+
+$db = new PDO('mysql:host=localhost;dbname=' . $db_name . ';charset=utf8', $db_user, $db_pass);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
